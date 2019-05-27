0ba7362ca5f971e3b3ab3954c2725b51cd30e735
masato.kanamori <masato.kanamori@rakus.co.jp>
2019-05-27 16:08:43 +0900

    SearchControllerの作成

--- src/main/java/com/example/controller/SearchController.java
+++ src/main/java/com/example/controller/SearchController.java
@@ -1,5 +1,26 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.repository.UserRepository;
+
+@Controller
+@RequestMapping("search")
 public class SearchController {
+	@Autowired
+	private UserRepository repository;
 	
+	@RequestMapping("")
+	public String index() {
+		return "user-info-search";
+	}
+	
+	@RequestMapping("/user-info-view")
+	public String search(Model model,Integer id) {
+		model.addAttribute("user", repository.load(id));
+		return "user-info-view";
+	}
 }
